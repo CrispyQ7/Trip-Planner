de0a75a36b9e0ec04ae48cdf7dfd9ad3b60b90a9
njasi <nick@jasinski3.com>
2019-09-30 11:29:33 -0500

    did fun stuff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('AM i running correctly?');
+
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibmphc2kiLCJhIjoiY2sxNmw3b3MwMHlmMDNjbGIweWljcXEzcyJ9.Tj5JyZqhw4ngd0-Vp9tSDA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v11" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+marker.addTo(map);
+
+const marker2 = buildMarker("restaurant", [-87.6354, 41.8885]); // or [-87.6354, 41.8885]
+marker2.addTo(map);
+
+module.exports = map
---
+++ src/marker.js
@@ -0,0 +1,27 @@
+const mapboxgl = require("mapbox-gl");
+
+function buildMarker(type,coords){
+  let mark = document.createElement("div");
+
+  mark.style.width = "32px";
+  mark.style.height = "39px";
+
+  switch(type){
+    default:
+    case "activity":
+      mark.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+      break;
+    case "hotel":
+      mark.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)"
+      break;
+    case "restaurant":
+      mark.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)"
+  }
+
+  const marker = new mapboxgl.Marker({
+    element:mark
+  }).setLngLat(coords)
+  return marker
+}
+
+module.exports = buildMarker;
